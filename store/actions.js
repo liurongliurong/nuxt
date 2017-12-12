259b9ca208150d979333103027aeaa2a3a92858b
elena <329655021@qq.com>
2017-12-12 20:37:08 +0800

    fetch is not correctly used

--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,6 @@
 export default {
   getInfo ({commit, state}) {
+    console.log(11)
     var data = localStorage.getItem('info')
     if (data) {
       state.info = Object.assign(state.info, JSON.parse(data))
