313d4896ea1c3a84886f023ad88c2c83f6495e88
elena <329655021@qq.com>
2017-12-13 16:57:39 +0800

    some change

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,7 @@ module.exports = {
     title: 'starter',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { name: 'viewport', content: 'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/logo_icon.png' },
