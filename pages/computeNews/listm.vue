dc24971538d6428c28594e6ecc84c25234e904a5
FuLiKai <695257958@qq.com>
2017-12-16 13:17:23 +0800

    资讯

--- pages/computeNews/listm.vue
+++ pages/computeNews/listm.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="mobilelists">
-    <div v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="len" class="list_lists" v-if="!showcontent">
-      <div v-for="item, k in museum" :key="k" @click="clickcontent(item.id)" class="list_list">
-        <div class="list_left">
-          <h3>{{item.title}}</h3>
-          <p>{{item.resume?item.resume: '暂无简介'}}</p>
-          <div><span></span>{{item.source?item.source: '算力网'}}</div>
+  <div class="listm">
+    <div class="mobile_computehome">
+        <div class="mobile_navcompute">
+        <router-link :to="n.path" v-for="n, k in navcompute" :key="k">
+            <em>{{n.title}}</em>
+            <span>|</span>
+        </router-link>
         </div>
-        <div class="list_right">
-          <img :src="item.image"/>
-          <div class="time">
-            <p><span class="icon iconfont icon-shijian2"></span>{{item.dateline.split(" ")[0].split("-")[1]}}月{{item.dateline.split(" ")[0].split("-")[2]}}日</p>
-            <p><span class="icon iconfont icon-yuedu1"></span>{{item.view_count?item.view_count : '0'}}</p>
-          </div>
-        </div>
-      </div>
     </div>
-    <p v-if="loading && !showcontent"  class="loadmore">加载中······</p>
-    <div class="quicknews_content"  v-if="showcontent">
-      <div class="title">
-        <span>{{content.title}}</span>
-        <a class="button" @click="showcontent1(false)">< 返回列表</a>
-      </div>
-      <div class="info_quick" v-html="content.content"></div>
+    <div class="mobilelists">
+        <div v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="len" class="list_lists" v-if="!showcontent">
+        <div v-for="item, k in museum" :key="k" @click="clickcontent(item.id)" class="list_list">
+            <div class="list_left">
+            <h3>{{item.title}}</h3>
+            <p>{{item.resume?item.resume: '暂无简介'}}</p>
+            <div><span></span>{{item.source?item.source: '算力网'}}</div>
+            </div>
+            <div class="list_right">
+            <img :src="item.image"/>
+            <div class="time">
+                <p><span class="icon iconfont icon-shijian2"></span>{{item.dateline.split(" ")[0].split("-")[1]}}月{{item.dateline.split(" ")[0].split("-")[2]}}日</p>
+                <p><span class="icon iconfont icon-yuedu1"></span>{{item.view_count?item.view_count : '0'}}</p>
+            </div>
+            </div>
+        </div>
+        </div>
+        <p v-if="loading && !showcontent"  class="loadmore">加载中······</p>
+        <div class="quicknews_content"  v-if="showcontent">
+        <div class="title">
+            <span>{{content.title}}</span>
+            <a class="button" @click="showcontent1(false)">< 返回列表</a>
+        </div>
+        <div class="info_quick" v-html="content.content"></div>
+        </div>
     </div>
   </div>
 </template>
@@ -43,7 +53,8 @@
         loading: false,
         museum: [],
         showcontent: false,
-        content: ''
+        content: '',
+        navcompute: [{title: '快讯', path: '/quickNews'}, {title: '资讯', path: '/computeNews/listm'}, {title: '测评', path: '/equipmentEvaluate/list'}, {title: '币种', path: '/digitalCurrency/list'}, {title: '厂商', path: '/manufacturer/list'}]
       }
     },
     methods: {
@@ -92,6 +103,69 @@
 </script>
 
 <style lang="scss">
+    .mobile_computehome{
+    width: 100%;
+    height: 100%;
+    .mobile_navcompute{
+      width: 100%;
+      height: 2rem;
+      background: white;
+      display: flex;
+      justify-content: space-between;
+      border-bottom:1px solid #bfbfbf;
+      box-sizing: border-box;
+      :last-child span{
+        display:none;
+      }
+      a{
+        width: 20%;
+        height: 2rem;
+        display:inline-block;
+        line-height: 2rem;
+        text-align: center;
+        span{
+            float: right;
+            font-size: 0.6rem;
+            color:#1b1b1b;
+        }
+        em{
+            font-style:normal;
+            font-size: 0.6rem;
+            color:#1b1b1b;
+        }
+        &:hover{
+            em{
+                display:inline-block;
+                border-bottom:2px solid #fe5039;
+                box-sizing: border-box;
+                width: 1.9rem;
+                height: 1.95rem;
+                color:#fe5039;
+            }
+        }
+        &.active{
+            em{
+                display:inline-block;
+                border-bottom:2px solid #fe5039;
+                box-sizing: border-box;
+                width: 1.9rem;
+                height: 1.95rem;
+                color:#fe5039;
+          }
+        }
+        &.nuxt-link-active{
+            em{
+                display:inline-block;
+                border-bottom:2px solid #fe5039;
+                box-sizing: border-box;
+                width: 1.9rem;
+                height: 1.95rem;
+                color:#fe5039;
+          }
+        }
+      }
+    }
+  }
   .mobilelists{
     width: 100%;
     overflow: hidden;
