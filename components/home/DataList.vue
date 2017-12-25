7fa829ad8ee21004fd383a6cd86eb2200bd83b86
elena <329655021@qq.com>
2017-12-25 15:27:53 +0800

    index layout

--- components/home/DataList.vue
+++ components/home/DataList.vue
@@ -1,77 +1,105 @@
 <template>
-  <div class="millsList" v-if="total">
-    <h5>
-      <span class="one">热门矿机推荐</span>
-      <span class="two">保全网提供全流程区块链存证、保全业务</span>
-      <router-link class="get_more" to="/minerShop/miner/1">更多矿机热门 ></router-link>
-    </h5>
-    <div class="mill">
-      <div v-for="n,k in minerData" class="listmill">
-        <a href="javascript:;" @click="goPay(n.id)">
-          <span class="status" v-if="n.status===1">热销中</span>
-          <span class="gray" v-if="n.status===3">已下架</span>
-          <span class="gray" v-if="n.status===2">已售罄 </span>
-          <span class="gray" v-if="n.status===4" style="background: #32cf99;">预热</span>
-          <div class="img1">
-            <img :src="n.minerPicture"/>
-          </div>
-          <h6>{{n.name}}</h6>
-          <p class="address"><span class="left">{{n.unit?n.unit: 'BitCoin'}}</span><span class="right">{{n.MinerAddress?n.MinerAddress: '未定'}}</span></p>
-          <div class="progress_info press">
-            <div class="progress_box">
-              <div class="box" :style="{width:(n.buyed_amount/n.amount * 100)+'%'}"></div>
+  <div class="cloud_list">
+    <div class="box" v-if="total">
+      <h1 class="home_item_title">全方位为你推荐算力服务器</h1>
+      <p class="home_item_desc">全球算力输出服务由保全网提供全流程区块链存证、保全服务</p>
+      <table>
+        <thead>
+          <tr>
+            <th v-for="n in nav">{{n.title}}</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="l,i in list1" @click="goPay(l.product_id||l.id, 2)">
+            <td v-for="v,k in nav">
+              <template v-if="k==='name'"><i class="iconfont">&#xe605;</i>{{l[k]}}</template>
+              <template v-else-if="k==='left_num'">{{l.amount-(l.sell_amount||l.buyed_amount)+v.unit}}</template>
+              <template v-else>{{l[k]+[v.unit]}}</template>
+            </td>
+            <td>
+              <a class="btn" v-if="l.status===4">预热</a> 
+              <a class="btn" v-else-if="l.amount-(l.sell_amount||l.buyed_amount)>0">立即购买</a> 
+              <button class="btn" disabled v-else>已售罄</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <router-link class="get_more" to="/minerShop/miner/2">查看更多云算力 ></router-link>
+    </div>
+    <div class="box">
+      <div class="miner_list_box">
+        <div v-for="n,k in minerData" class="listmill">
+          <a href="javascript:;" @click="goPay(n.id)">
+            <span class="status" v-if="n.status===1">热销中</span>
+            <span class="gray" v-if="n.status===3">已下架</span>
+            <span class="gray" v-if="n.status===2">已售罄 </span>
+            <span class="gray" v-if="n.status===4" style="background: #32cf99;">预热</span>
+            <div class="img1">
+              <img :src="n.minerPicture"/>
             </div>
-          </div>
-          <div class="items">
-            <div class="item" v-for="item,d in items">
-              <p class="price" v-if="d==='buyed_amount'">{{n.amount-n.buyed_amount}}{{item.unit}}</p>
-              <p class="price" v-else>{{n[d]}}{{item.unit}}</p>
-              <p class="title">{{item.title}}</p>
+            <h6>{{n.name}}</h6>
+            <p class="address"><span class="left">{{n.unit?n.unit: 'BitCoin'}}</span><span class="right">{{n.MinerAddress?n.MinerAddress: '未定'}}</span></p>
+            <div class="progress_info press">
+              <div class="progress_box">
+                <div class="box" :style="{width:(n.buyed_amount/n.amount * 100)+'%'}"></div>
+              </div>
             </div>
-          </div>
-        </a>
+            <div class="items">
+              <div class="item" v-for="item,d in items">
+                <p class="price" v-if="d==='buyed_amount'">{{n.amount-n.buyed_amount}}{{item.unit}}</p>
+                <p class="price" v-else>{{n[d]}}{{item.unit}}</p>
+                <p class="title">{{item.title}}</p>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
+      <router-link class="get_more" to="/minerShop/miner/1">查看更多矿机 ></router-link>
     </div>
   </div>
 </template>
-
 
 <script>
   import api from '@/util/function'
   import util from '@/util'
   import { mapState } from 'vuex'
-  import PcDatalist from './pc'
-  import MDatalist from './mobile'
   export default {
-    name: 'datalist',
+    name: 'cloud_list',
     data () {
       return {
-        items: {'one_amount_value': {title: '矿机单价', unit: '元'}, 'hash': {title: '算力', unit: 'T'}, 'buyed_amount': {title: '剩余数量', unit: '台'}},
-        nav: {'name': {title: '矿机名称', unit: ''}, 'amount': {title: '总数量', unit: '台'}, 'one_amount_value': {title: '单价', unit: '元'}, 'hash': {title: '算力', unit: 'T'}, 'left_num': {title: '剩余数量', unit: '台'}},
+        nav: {'name': {title: '云算力名称', unit: ''}, 'amount': {title: '总数量', unit: '台'}, 'one_amount_value': {title: '单价', unit: '元'}, 'hash': {title: '算力', unit: 'T'}, 'power': {title: '功耗', unit: 'W/T'}, 'type_name': {title: '算力类型', unit: ''}, 'left_num': {title: '剩余数量', unit: '台'}},
         list: [],
-        minerData: [],
-        total: ''
+        list1: [],
+        index: '',
+        total: '',
+        items: {'one_amount_value': {title: '矿机单价', unit: '元'}, 'hash': {title: '算力', unit: 'T'}, 'buyed_amount': {title: '剩余数量', unit: '台'}},
+        minerData: []
       }
     },
     methods: {
-      goPay (id) {
-        localStorage.setItem('params', JSON.stringify([ id, '1']))
+      goPay (id, type) {
+        localStorage.setItem('params', JSON.stringify([ id, type]))
         this.$router.push({path: '/minerShop/detail/'})
       }
     },
     mounted () {
       var self = this
-      util.post('showTopMiner', {sign: api.serialize({token: this.token})}).then(function (res) {
+      util.post('product_top_list', {sign: api.serialize({token: this.token})}).then(function (res) {
         api.checkAjax(self, res, () => {
-          self.minerData = res
+          self.list1 = res
           self.total = res.length
         })
       }).catch(res => {
         console.log(res)
       })
-    },
-    filters: {
-      format: api.readable
+      util.post('showTopMiner', {sign: api.serialize({token: this.token})}).then(function (res) {
+        api.checkAjax(self, res, () => {
+          self.minerData = res
+        })
+      }).catch(res => {
+        console.log(res)
+      })
     },
     computed: {
       ...mapState({
@@ -82,24 +110,147 @@
 </script>
 
 <style type="text/css" lang="scss">
-  @import '../../../assets/css/style.scss';
+  @import '../../assets/css/style.scss';
   .cloud_list{
     overflow: hidden;
     background: #F6F7FB;
-    .btn{
-      border:0;
-      width:145px;
-      color: $orange;
-      text-align: center;
-      line-height: 48px;
-      border-radius:5px;
-      background: transparent;
-      &:disabled{
-        cursor: no-drop;
-        color:$light_black
-      }
-    }
     .box{
+      .miner_list_box{
+        @include main
+        overflow:hidden;
+        .listmill{
+          width: 280px;
+          height: 320px;
+          background: white;
+          float: left;
+          text-align: center;
+          margin-left: 8px;
+          margin-right: 11px;
+          position: relative;
+          .img1{
+            width: 100%;
+            height: 180px;
+            position: relative;
+            img{
+              width: 126px;
+              height: 81px;
+              left: 50%;
+              margin-left: -63px;
+              object-fit: cover;
+              top:64px;
+              position: absolute;
+            }
+          }
+          .status{
+            width: 70px;
+            height: 25px;
+            display: block;
+            text-align: center;
+            line-height: 25px;
+            background: #ff6458;
+            color:white;
+            font-size: 12px;
+            position: absolute;
+            left: 0;
+            left:0;
+          }
+          .gray{
+            background: #bfbfbf;
+            width: 70px;
+            height: 25px;
+            display: block;
+            text-align: center;
+            line-height: 25px;
+            color:white;
+            font-size: 12px;
+            position: absolute;
+            left: 0;
+            top:0;
+          }
+          h6{
+            font-size: 16px;
+            color:#121212;
+            padding-left: 20px;
+            padding-right: 20px;
+            box-sizing: border-box;
+            text-align: left;
+          }
+          .address{
+            width: 100%;
+            padding:0 20px;
+            box-sizing: border-box;
+            padding-top: 9px;
+            overflow: hidden;
+            .left{
+              color: #327fff;
+              float: left;
+              font-size: 12px;
+            }
+            .right{
+              float: right;
+              color: #a9a9a9;
+              font-size: 12px;
+            }
+          }
+          .press{
+            width: 240px;
+            height: 5px;
+            background: #e3e3e3;
+            margin: 0 auto;
+            margin-top: 20px;
+            .progress_box{
+              position: relative;
+              height:100%;
+              .box{
+                @include position
+                background: #32cf99;
+              }
+            }
+          }
+          .items{
+            width: 100%;
+            padding: 0 20px;
+            padding-top: 18px;
+            .item{
+              width: 33.3%;
+              float: left;
+              text-align: center;
+              p{
+                margin:0;
+                padding:0;
+              }
+              .price{
+                height: 20px;
+                font-size: 14px;
+                color: #666666;
+                text-align: left;
+              }
+              .title{
+                color: #a9a9a9;
+                font-size: 12px;
+                text-align: left;
+              }
+            }
+            :nth-child(1){
+              width: 80px;
+              height: 30px;
+            }
+            :nth-child(2){
+              padding-left:35px;
+            }
+            :nth-child(3){
+              padding-left: 30px;
+            }
+          }
+          &:hover{
+            box-shadow:#dfe0e1 0 0 30px;
+            z-index: 999;
+            img{
+              transform: scale(1.1);
+            }
+          }
+        }
+      }
       table{
         @include table;
         @include main;
@@ -146,261 +297,9 @@
       }
       .get_more{
         text-align: center;
-        margin:37px 0;
+        margin:20px 0;
         display: block;
-        color:$light_black;
-      }
-    }
-  }
-  .millsList{
-    width: 100%;
-    overflow: hidden;
-    margin:0 auto;
-    background: #f6f7fb;
-    margin-top: 0 !important;
-    h5{
-      width: 1180px !important;
-      margin:0 auto;
-      margin-bottom: 20px !important;
-      text-align: left;
-      .one{
-        font-size: 28px !important;
-        color: #002059;
-      }
-      .two{
-        font-size: 18px !important;
-        color: #002059;
-        margin-left: 50px !important;
-      }
-      a{
-        float: right;
-        font-size: 18px;
-        color: #327fff;
-        padding-top: 10px;
-      }
-    }
-    .mill{
-      width: 1198px;
-      // overflow: hidden;
-      margin:0 auto;
-      .listmill{
-        padding:0;
-        margin:0;
-        width: 280px;
-        height: 320px;
-        background: white;
-        float: left;
-        text-align: center;
-        margin-bottom: 24px;
-        margin-left: 8px;
-        margin-right: 11px;
-        position: relative;
-        .img1{
-          width: 100%;
-          height: 180px;
-          position: relative;
-          img{
-            width: 126px;
-            height: 81px;
-            left: 50%;
-            margin-left: -63px;
-            object-fit: cover;
-            top:64px;
-            position: absolute;
-          }
-        }
-        .status{
-          width: 70px;
-          height: 25px;
-          display: block;
-          text-align: center;
-          line-height: 25px;
-          background: #ff6458;
-          color:white;
-          font-size: 12px;
-          position: absolute;
-          left: 0;
-          left:0;
-        }
-        .gray{
-          background: #bfbfbf;
-          width: 70px;
-          height: 25px;
-          display: block;
-          text-align: center;
-          line-height: 25px;
-          color:white;
-          font-size: 12px;
-          position: absolute;
-          left: 0;
-          top:0;
-        }
-        h6{
-            font-size: 16px;
-            color:#121212;
-            padding-left: 20px;
-            padding-right: 20px;
-            box-sizing: border-box;
-            text-align: left;
-        }
-        .address{
-            width: 100%;
-            padding:0 20px;
-            box-sizing: border-box;
-            padding-top: 9px;
-            overflow: hidden;
-            .left{
-                color: #327fff;
-                float: left;
-                font-size: 12px;
-            }
-            .right{
-                float: right;
-                color: #a9a9a9;
-                font-size: 12px;
-            }
-        }
-        .press{
-            width: 240px;
-            height: 5px;
-            background: #e3e3e3;
-            margin: 0 auto;
-            margin-top: 20px;
-            .progress_box{
-              position: relative;
-              // overflow:hidden;
-              height:100%;
-              .box{
-                @include position
-                background: #32cf99;
-              }
-            }
-        }
-        .items{
-            width: 100%;
-            padding: 0 20px;
-            padding-top: 18px;
-            .item{
-                width: 33.3%;
-                float: left;
-                text-align: center;
-                p{
-                    margin:0;
-                    padding:0;
-                }
-                .price{
-                  height: 20px;
-                  font-size: 14px;
-                  color: #666666;
-                  text-align: left;
-                }
-                .title{
-                    color: #a9a9a9;
-                    font-size: 12px;
-                    text-align: left;
-                }
-            }
-            :nth-child(1){
-                width: 80px;
-                height: 30px;      
-            }
-            :nth-child(2){
-                padding-left:35px;
-                // margin-right:5px;
-            }
-            :nth-child(3){
-              padding-left: 30px;
-            }
-        }
-        &:hover{
-           box-shadow:#dfe0e1 0 0 30px;
-           z-index: 999;
-           img{
-             transform: scale(1.1);
-           }
-        }
-      }
-      .nodata{
-        background: #fff;
-        min-height:500px;
-        padding-top:100px;
-        text-align: center;
-        .nodata_img{
-          display: inline-block;
-          width: 305px;
-          height: 234px;
-          background: url('../../../assets/images/css_sprites.png') -10px -10px;
-        }
-        p{
-          color:$light_black;
-          margin-top:15px
-        }
-      }
-    }
-    .mobileminer{
-      width: 100%;
-      overflow: hidden;
-      padding:0 .5rem;
-      background: white;
-      padding-top: .5rem;
-      min-height: 100vh;
-      .list_lists{
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        flex-wrap: wrap;
-      }
-      .millsList_mobile{
-        display: block;
-        width: 48%;
-        height: 7.5rem;
-        overflow: hidden;
-        a{
-          .null{
-            width: 100%;
-            height: 4.3rem;
-            background: #efefef;
-            border-radius: .3rem;
-            text-align: center;
-            img{
-              width: 4rem;
-              height: 2.4rem;
-              margin-top: 0.9rem;
-            }
-          }
-          h6{
-            width: 100%;
-            text-overflow: ellipsis;
-            overflow: hidden;
-            white-space: normal;
-            font-size: .6rem;
-            padding-top: .2rem;
-          }
-          .progress_info1{
-            position: relative;
-            padding:6px 0;
-            .progress_box1{
-              position: relative;
-              overflow:hidden;
-              border-radius:5px;
-              height:5px;
-              background: $border;
-              .box1{
-                @include position
-                @include process_color
-              }
-            }
-          }
-          p{
-            font-size: 0.4rem;
-            b{
-              color:#fe5039;
-            }
-            span{
-              float: right;
-            }
-          }
-        }
+        color: #327fff !important;
       }
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,18 @@
       <div class="home_text">
         <div class="main">
           <div class="list">
-            <div class="item" v-for="a,k in ad.items">
-              <img :src="a.img1"/>
+            <div class="item" v-for="k in 4">
+              <img :src="require('@/assets/images/home/feature'+k+'.png')"/>
             </div>
           </div>
         </div>
       </div>
-      <minner-list></minner-list>
-      <MyData></MyData>
+      <DataList></DataList>
       <WebInfo></WebInfo>
       <div class="home_title">
         <div class="main">
-          <h1>{{ad.title}}</h1>
-          <p>{{ad.desc}}</p>
+          <h1>算力驱动未来，信任链接天下</h1>
+          <p>全球算力产业链资源整合，基于区块链的分布式算力输出平台</p>
           <div class="list">
             <div class="item" v-for="s,k in suanLi">
               <div class="blurborder">
@@ -70,8 +69,7 @@
 <script>
   import util from '@/util'
   import api from '@/util/function'
-  import MyData from '@/components/home/dataList'
-  import MinnerList from '@/components/home/MinerList'
+  import DataList from '@/components/home/DataList'
   import Swiper from '@/components/common/Swipe'
   import WebInfo from '@/components/home/WebInfo'
   import SideBar from '@/components/home/SideBar'
@@ -79,15 +77,11 @@
   import { mapState } from 'vuex'
   export default {
     components: {
-      Swiper, MyData, WebInfo, SideBar, DataMap, MinnerList
+      Swiper, DataList, WebInfo, SideBar, DataMap
     },
     data () {
       return {
-        nav: [{title: '品牌矿机', desc: '无忧购买矿机', url: '/minerShop/miner/1'}, {title: '云算力', desc: '掌握产业动态', url: '/minerShop/miner/2'}, {title: 'BDC托管', desc: '多个BDC中心', url: '/bdc'}, {title: '产业资讯', desc: '掌握产业动态', url: '/quickNews'}],
-        data: [1, 1, 1],
         link: [{route: 'http://www.tsinghua-zj.edu.cn/'}, {route: 'http://www.gosun.com/'}, {route: 'http://www.enjoyor.cc/'}, {route: 'https://canaan.io/'}, {route: 'http://www.ebang.com.cn/'}, {route: 'https://www.suanlibao.com/'}],
-        newsNav: [{title: '热点快讯·不止于此', desc: '开启算力新篇章'}, {title: '实时交易信息', desc: '前往了解更多'}, {title: '挖矿币种资料', desc: '前往了解更多'}],
-        ad: {title: '算力驱动未来，信任链接天下', desc: '全球算力产业链资源整合，基于区块链的分布式算力输出平台', items: [{img1: require('@/assets/images/home/feature1.png')}, {img1: require('@/assets/images/home/feature2.png')}, {img1: require('@/assets/images/home/feature3.png')}, {img1: require('@/assets/images/home/feature4.png')}]},
         suanLi: [{title: 'SHA256比特币算力', desc: 'Bitcoin数字货币算力', bardesc: ''}, {title: '卷积神经算法算力', desc: '为CNN卷积神经', bardesc: '网络提供分布式加速服务'}, {title: 'EquiHash零币算力', desc: 'ZeroCASH提供隐私保护', bardesc: '及零知识证明的基础算力'}, {title: '智能合约算力', desc: '全球贸易智能合约', bardesc: '服务的分布式基础算力'}, {title: 'Curecoin算力', desc: '蛋白质折叠计算，', bardesc: '生化反应模型，用于发现新药'}, {title: '游戏币兑换算力', desc: '全球游戏产业', bardesc: '虚拟货币通用兑换算力'}, {title: 'Ethash以太算力', desc: '以太坊网络', bardesc: 'ETC，ETH算力'}, {title: '公证算力', desc: '提供区块链公证服务', bardesc: '存证保全的基础算力'}],
         mapData: [{title: '全网算力', name: 'hashrate', unit: 'PH/s'}, {title: '全网困难度', name: 'difficulty', unit: 'T'}],
         computeData: {},
@@ -99,12 +93,10 @@
       goMobile () {
         if (api.checkEquipment()) {
           this.$store.commit('SET_EQUIPMENT', 1)
+          this.$router.replace({path: '/mobileIndex'})
         } else {
           this.$store.commit('SET_EQUIPMENT', 0)
         }
-      },
-      goPage (url, k) {
-        this.$router.push({path: url})
       },
       getComputeData () {
         util.post('showDifficulty', {sign: 'token=0'})
