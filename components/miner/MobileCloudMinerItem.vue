e7be9cab54e81558631dd1d32b0fcfedfa2476ce
FuLiKai <695257958@qq.com>
2017-12-28 20:00:45 +0800

    算力

--- components/miner/MobileCloudMinerItem.vue
+++ components/miner/MobileCloudMinerItem.vue
@@ -14,7 +14,7 @@
           <div class="mobile_text_item">剩余可售<b>{{d.amount-d.buyed_amount}}台</b></div>
         </div>
       </div>
-      <div class="circle sell_progress" v-if="d.buyed_amount === '0.00'" style="background:#e5e5e5;">
+      <!-- <div class="circle sell_progress" v-if="d.buyed_amount === '0.00'" style="background:#e5e5e5;">
         <template v-if="(((d.buyed_amount)/d.amount*100).toFixed(0))<=180">
             <div class="pie_left" style="clip: rect(0,-360px,auto,0);"><div class="left"></div></div>
           <div class="pie_right" style="clip: rect(0,auto,auto,360px);"><div class="right"  :style="{transform:'rotate(-'+(((d.buyed_amount)/d.amount*100).toFixed(0) * 3.6)+'deg)'}"></div></div>
@@ -35,13 +35,19 @@
             <div class="pie_right"><div class="right" :style="{transform:'rotate('+180+'deg)'}"></div></div>
         </template>
         <div class="mask"  v-if="d.buyed_amount === '0.00'"  style="color:#e5e5e5;"><span style="color:#e5e5e5;">{{((d.buyed_amount)/d.amount*100).toFixed(0)}}</span>%</div>
-        <div class="mask" v-else><span>{{((d.buyed_amount)/d.amount*100).toFixed(0)}}</span>%</div>
-      </div>
+        <div class="mask" v-else><span>{{(}}</span>%</div>
+      </div> -->
+      <el-progress type="circle" :percentage="((d.buyed_amount)/d.amount*100).toFixed(0)" style="    height: 76px !important;
+    width: 76px !important;"></el-progress>
     </div>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import ElementUI from 'element-ui'
+  import 'element-ui/lib/theme-chalk/index.css'
+  Vue.use(ElementUI)
   export default {
     props: {
       d: {
@@ -141,5 +147,18 @@
         }
       }
     }
+  }
+  .el-progress-circle{
+    width: 76px !important;
+    height:  76px !important;
+    position: relative;
+    top: -1rem;
+  }
+  .el-progress__text{
+    position: relative !important;
+    top: -60px !important;
+  }
+  .el-progress-circle__path{
+    stroke:#ffb386 !important;
   }
 </style>
