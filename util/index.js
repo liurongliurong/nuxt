2ee31ec4bf7ea7271798270c309e959b5ff34f04
zxw <18768074437@163.com>
2018-01-02 17:27:58 +0800

    算力

--- util/index.js
+++ util/index.js
@@ -4,7 +4,6 @@ import qs from 'qs'
 import util from './function'
 
 let api = axios.create({
-<<<<<<< HEAD
   // baseURL: 'https://www.suanli.com/background/api',
   baseURL: 'http://192.168.3.45/background/api/',
   // baseURL: 'http://suanli.baoquan.com/background/api',
