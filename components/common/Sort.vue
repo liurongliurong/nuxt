1643eb78ea093b1a165f3620b5e72bbf50c08925
FuLiKai <695257958@qq.com>
2018-01-05 20:12:43 +0800

    算力样式

--- components/common/Sort.vue
+++ components/common/Sort.vue
@@ -213,17 +213,12 @@
         line-height: 20px;
         text-align: center;
         vertical-align: text-bottom;
-        transition:all .3s;
         font-size: 26px;
         transform:rotate(180deg);
-        position: relative;
-        top: 6px;
       }
       &.active{
         color:$blue;
         .iconfont{
-          position: relative;
-          top: -6px;
         }
       }
       &.up{
