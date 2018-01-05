f331d2145873f50a176565bae4fa6cbaf249e917
FuLiKai <695257958@qq.com>
2018-01-05 20:23:28 +0800

    sort style

--- components/common/Sort.vue
+++ components/common/Sort.vue
@@ -210,15 +210,20 @@
       .iconfont{
         @include block(24)
         height:16px;
-        line-height: 20px;
+        line-height: 40px;
         text-align: center;
         vertical-align: text-bottom;
         font-size: 26px;
+        transition: all .3s;
         transform:rotate(180deg);
+        position: relative;
+        top: -4px;
       }
       &.active{
         color:$blue;
         .iconfont{
+          position: relative;
+          top: 2px;
         }
       }
       &.up{
