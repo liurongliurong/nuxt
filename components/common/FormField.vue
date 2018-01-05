5e2ca1bf22218d95413058bd7805a5c2c9b9977e
elena <329655021@qq.com>
2018-01-05 19:55:28 +0800

    alipay

--- components/common/FormField.vue
+++ components/common/FormField.vue
@@ -44,10 +44,7 @@
             </select>
           </div>
         </template>
-        <template v-else>
-          <input :type="f.type" :name="f.name" :isChange="f.isChange" disabled v-if="f.name==='open_bank'">
-          <input :type="f.type" :name="f.name" :value="$parent[f.name]||$parent.$parent[f.name]" disabled :isChange="f.isChange" v-else>
-        </template>
+        <input :type="f.type" :name="f.name" :value="$parent[f.name]||$parent.$parent[f.name]||''" disabled :isChange="f.isChange" v-else>
         <!-- tips_info -->
         <div class="tips_info" v-if="f.tipsInfo">
           <template v-if="f.tipsUnit==='hash'">
